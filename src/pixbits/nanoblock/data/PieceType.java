5600cb9792b698ca0ecc9d1514df088dfa35baf2
Jacopo Santoni <jack@pixbits.com>
2013-12-12 05:00:06 +0000

    implementato pezzo round

--- src/pixbits/nanoblock/data/PieceType.java
+++ src/pixbits/nanoblock/data/PieceType.java
@@ -38,7 +38,7 @@ public class PieceType
   public final static PieceType P2x4 = new PieceType(2,4,false);
   
   public final static PieceType[] pieces = new PieceType[] {
-    P1x1, P2x1, P1x2, P2x2
+    P1x1, P1x1r, P2x1, P1x2, P2x2
   };
   
   public static int count() { return pieces.length; }
