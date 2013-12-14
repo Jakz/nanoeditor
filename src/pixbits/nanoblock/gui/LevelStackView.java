14d7f68a133e2438e5c9e89f4c82f892d2bdd142
Jacopo Santoni <jack@pixbits.com>
2013-12-14 06:25:04 +0000

    LevelStackView con hover multiplo + prove processing gl ma è lentissimo

---
+++ src/pixbits/nanoblock/gui/LevelStackView.java
@@ -0,0 +1,38 @@
+package pixbits.nanoblock.gui;
+
+import pixbits.nanoblock.data.*;
+import pixbits.nanoblock.gui.ui.*;
+
+import java.awt.Rectangle;
+
+public class LevelStackView
+{
+  LevelView[] views;
+  LevelScrollBar scrollbar;
+  
+  private Rectangle hover;
+  
+  LevelStackView(Sketch p, int count, int ox, int oy, int cellSize, int margin, Model model)
+  {
+    views = new LevelView[count];
+    
+    for (int i = 0; i < count; ++i)
+    {
+      views[i] = new LevelView(this, p, model, model.levelAt(i), ox, oy+(count-i-1)*(margin+cellSize*model.height), cellSize);
+      p.addDrawable(views[i]);
+    }
+    
+    LevelScrollBar levelScrollBar = new LevelScrollBar(p, model, views, ox+cellSize*model.width, oy, 20, model.height*cellSize*count + margin*(count-1), 20);
+    p.addDrawable(levelScrollBar);
+  }
+  
+  public Rectangle hover()
+  {
+    return hover;
+  }
+  
+  public void setHover(Rectangle r)
+  {
+    hover = r;
+  }
+}
