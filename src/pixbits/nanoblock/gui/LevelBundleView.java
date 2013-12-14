85c1727c4bd73ca067023a1917874bc37e2414e0
Jacopo Santoni <jack@pixbits.com>
2013-12-14 04:49:24 +0000

    LevelBundleView stubbata

---
+++ src/pixbits/nanoblock/gui/LevelBundleView.java
@@ -0,0 +1,15 @@
+package pixbits.nanoblock.gui;
+
+import pixbits.nanoblock.data.*;
+import pixbits.nanoblock.gui.ui.*;
+
+public class LevelBundleView
+{
+  LevelView[] views;
+  LevelScrollBar scrollbar;
+  
+  LevelBundleView(int count, int ox, int oy, int cellSize, int margin, Model model)
+  {
+    
+  }
+}
