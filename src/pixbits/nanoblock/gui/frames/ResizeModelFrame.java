d208c86ed51c11d21e72874c2d8bb214c5e23e21
Jack <jack@pixbits.com>
2014-10-17 04:29:23 +0200

    finished layout of ResizeModelFrame and basic UI behavior

--- src/pixbits/nanoblock/gui/frames/ResizeModelFrame.java
+++ src/pixbits/nanoblock/gui/frames/ResizeModelFrame.java
@@ -27,12 +27,33 @@ public class ResizeModelFrame extends JFrame
   private final AttachSide[][] buttons;
   private final JButton cancel, execute;
   
+  private final JLabel labelSize;
+  private final JLabel labelBounds;
+  private final JTextField fieldSize;
+  private final JTextField fieldBounds;
+  
+  private final JTextField width, height;
+  
+  private final JCheckBox cropToBounds;
+  private final JCheckBox keepCentered;
+  
+  private Dimension size = new Dimension(20,20);
+  private Dimension bounds = new Dimension(14,12);
+  
   public ResizeModelFrame()
   {
     JPanel gridPanel = new JPanel(new GridLayout(3,3));
-    gridPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(20, 0, 10, 0), BorderFactory.createTitledBorder("Side to attach")));
+    gridPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 10, 0));
     
     JPanel lowerPanel = new JPanel(new GridLayout(1,2));
+    lowerPanel.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+    
+    JPanel sizePanel = new JPanel(new GridLayout(2,2));
+    sizePanel.setBorder(BorderFactory.createTitledBorder("Current Size"));
+    
+    JPanel resizePanel = new JPanel();
+    resizePanel.setBorder(BorderFactory.createTitledBorder("Resize To"));
+
     
     group = new ButtonGroup();
     buttons = new AttachSide[3][3];
@@ -63,8 +84,50 @@ public class ResizeModelFrame extends JFrame
     lowerPanel.add(cancel);
     lowerPanel.add(execute);
     
+    labelSize = new JLabel("Model Size");
+    labelBounds = new JLabel("Model Bounds");
+    
+    fieldSize = new JTextField(size.width+"x"+size.height);
+    fieldSize.setHorizontalAlignment(JTextField.CENTER);
+    fieldSize.setEditable(false);
+    
+    fieldBounds = new JTextField(bounds.width+"x"+bounds.height);
+    fieldBounds.setHorizontalAlignment(JTextField.CENTER);
+    fieldBounds.setEditable(false);
+    
+    sizePanel.add(labelSize);
+    sizePanel.add(fieldSize);
+    sizePanel.add(labelBounds);
+    sizePanel.add(fieldBounds);
+
+    width = new JTextField(4);
+    height = new JTextField(4);
+    
+    JPanel panel1 = new JPanel();
+    
+    panel1.add(width);
+    panel1.add(new JLabel("x"));
+    panel1.add(height);
+    
+    resizePanel.setLayout(new BoxLayout(resizePanel, BoxLayout.PAGE_AXIS));
+    resizePanel.add(panel1);
+    
+    JPanel panel2 = new JPanel(new GridLayout(2,1));
+    cropToBounds = new JCheckBox("Crop to bounds");
+    keepCentered = new JCheckBox("Keep model centered");
+    cropToBounds.setAlignmentX(0.0f);
+    keepCentered.setAlignmentX(0.0f);
+    cropToBounds.addActionListener(listener);
+    keepCentered.addActionListener(listener);
+    
+    panel2.add(cropToBounds);
+    panel2.add(keepCentered);
+    resizePanel.add(panel2);
+    resizePanel.add(gridPanel);
+    
     this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-    this.add(gridPanel);
+    this.add(sizePanel);
+    this.add(resizePanel);
     this.add(lowerPanel);
     
     
@@ -75,12 +138,59 @@ public class ResizeModelFrame extends JFrame
   private final ActionListener listener = new ActionListener() {
     public void actionPerformed(ActionEvent e)
     {
-      JButton src = (JButton)e.getSource();
-      
-      if (src == cancel)
+      if (e.getSource() instanceof JButton)
       {
-        ResizeModelFrame.this.setVisible(false);
-        buttons[1][1].button.setSelected(true);
+        JButton src = (JButton)e.getSource();
+        if (src == cancel)
+        {
+          ResizeModelFrame.this.setVisible(false);
+          buttons[1][1].button.setSelected(true);
+        }
+      }
+      else if (e.getSource() instanceof JCheckBox)
+      {
+        JCheckBox box = (JCheckBox)e.getSource();
+        
+        if (box == cropToBounds)
+        {
+          if (box.isSelected())
+          {
+            width.setText(""+bounds.width);
+            height.setText(""+bounds.height);
+            width.setEditable(false);
+            height.setEditable(false);
+            keepCentered.setEnabled(false);
+            keepCentered.setSelected(false);
+            
+            for (int y = 0; y < 3; ++y)
+              for (int x = 0; x < 3; ++x)
+                buttons[y][x].button.setEnabled(false);
+          }
+          else
+          {
+            width.setEditable(true);
+            height.setEditable(true);
+            
+            for (int y = 0; y < 3; ++y)
+              for (int x = 0; x < 3; ++x)
+                buttons[y][x].button.setEnabled(true);
+            
+            if (buttons[1][1].button.isSelected())
+              keepCentered.setEnabled(true);
+          }
+        }
+      }
+      else if (e.getSource() instanceof JToggleButton)
+      {
+        JToggleButton src = (JToggleButton)e.getSource();
+        
+        if (src == buttons[1][1].button && src.isSelected())
+          keepCentered.setEnabled(true);
+        else
+        {
+          keepCentered.setEnabled(false);
+          keepCentered.setSelected(false);
+        }
       }
     }
   };
