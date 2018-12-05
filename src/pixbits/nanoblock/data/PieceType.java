165ce53f3fb716eb998e5bdac16cd0a050371aab
Jack <jack@pixbits.com>
2018-12-05 14:27:39 +0100

    moved PieceType names inside the type itself

--- src/pixbits/nanoblock/data/PieceType.java
+++ src/pixbits/nanoblock/data/PieceType.java
@@ -9,6 +9,7 @@ import com.pixbits.lib.functional.IntBiConsumer;
 
 public class PieceType
 {
+  public final String ident;
   public final int width, height;
   public final boolean rounded;
   
@@ -21,8 +22,9 @@ public class PieceType
   
   private final PieceOutline outline;
 
-  PieceType(int width, int height, boolean rounded, int[][] parts, int[][] caps, int[] outline)
+  PieceType(String ident, int width, int height, boolean rounded, int[][] parts, int[][] caps, int[] outline)
   {
+    this.ident = ident;
     this.width = width;
     this.height = height;
     this.rounded = rounded;
@@ -118,9 +120,9 @@ public class PieceType
       }
   }
   
-  PieceType(int width, int height, boolean rounded)
+  PieceType(String ident, int width, int height, boolean rounded)
   {
-    this(width, height, rounded, null, null, null);
+    this(ident, width, height, rounded, null, null, null);
   }
   
   public int maxWidth() { return width; }
@@ -153,53 +155,60 @@ public class PieceType
   public int maskSouth(int ox, int oy) { return masks[1][ox][oy]; }
   public int maskEast(int ox, int oy) { return masks[2][ox][oy]; }
   
-  public final static PieceType CAP    = new PieceType(1, 1, false);
+  public final static PieceType CAP    = new PieceType("cap", 1, 1, false);
   
-  public final static PieceType P1x1   = new PieceType(1, 1, false);
-  public final static PieceType P1x1r  = new PieceType(1, 1, true);
+  public final static PieceType P1x1   = new PieceType("1x1", 1, 1, false);
+  public final static PieceType P1x1r  = new PieceType("1x1r", 1, 1, true);
   
-  public final static PieceType P2x1   = new PieceType(2, 1, false);
-  public final static PieceType P1x2   = new PieceType(1, 2, false);
+  public final static PieceType P2x1   = new PieceType("2x1", 2, 1, false);
+  public final static PieceType P1x2   = new PieceType("1x2", 1, 2, false);
   
-  public final static PieceType P2x1c  = new PieceType(2, 1, false, null, new int[][] { { 1, 0 } }, null);
-  public final static PieceType P1x2c  = new PieceType(1, 2, false, null, new int[][] { { 0, 1 } }, null);
+  public final static PieceType P2x1c  = new PieceType("2x1c", 2, 1, false, null, new int[][] { { 1, 0 } }, null);
+  public final static PieceType P1x2c  = new PieceType("1x2c", 1, 2, false, null, new int[][] { { 0, 1 } }, null);
   
-  public final static PieceType P2x1r  = new PieceType(2, 1, true);
-  public final static PieceType P1x2r  = new PieceType(1, 2, true);
+  public final static PieceType P2x1r  = new PieceType("2x1r", 2, 1, true);
+  public final static PieceType P1x2r  = new PieceType("1x2r", 1, 2, true);
   
-  public final static PieceType P3x1   = new PieceType(3, 1, false);
-  public final static PieceType P1x3   = new PieceType(1, 3, false);
+  public final static PieceType P3x1   = new PieceType("3x1", 3, 1, false);
+  public final static PieceType P1x3   = new PieceType("1x3", 1, 3, false);
   
-  public final static PieceType P3x1r  = new PieceType(3, 1, true);
-  public final static PieceType P1x3r  = new PieceType(1, 3, true);
+  public final static PieceType P3x1r  = new PieceType("3x1r", 3, 1, true);
+  public final static PieceType P1x3r  = new PieceType("1x3r", 1, 3, true);
 
   
-  public final static PieceType P4x1   = new PieceType(4, 1, false);
-  public final static PieceType P4x1r  = new PieceType(4, 1, true);
-  public final static PieceType P1x4   = new PieceType(1, 4, false);
-  public final static PieceType P1x4r  = new PieceType(1, 4, true);
+  public final static PieceType P4x1   = new PieceType("4x1", 4, 1, false);
+  public final static PieceType P4x1r  = new PieceType("4x1r", 4, 1, true);
+  public final static PieceType P1x4   = new PieceType("1x4", 1, 4, false);
+  public final static PieceType P1x4r  = new PieceType("1x4r", 1, 4, true);
   
-  public final static PieceType P6x1   = new PieceType(6, 1, false);
-  public final static PieceType P1x6  = new PieceType(1, 6, false);
+  public final static PieceType P6x1   = new PieceType("6x1", 6, 1, false);
+  public final static PieceType P1x6   = new PieceType("1x6", 1, 6, false);
   
-  public final static PieceType P2x2   = new PieceType(2, 2, false);
-  public final static PieceType P2x2c  = new PieceType(2, 2, false, null, new int[][] { { 1, 1 } }, null);
+  public final static PieceType P2x2   = new PieceType("2x2", 2, 2, false);
+  public final static PieceType P2x2c  = new PieceType("2x2c", 2, 2, false, null, new int[][] { { 1, 1 } }, null);
   
-  public final static PieceType P2x2lt = new PieceType(2, 2, false, new int[][] { { 0, 0 }, { 1, 0 }, { 0, 1 } }, null, new int[] { 2, 1, -1, 1, -1 });
-  public final static PieceType P2x2lb = new PieceType(2, 2, false, new int[][] { { 1, 0 }, { 0, 1 }, { 1, 1 } }, null, new int[] { 1, -1, 1, 2, -2 });
+  public final static PieceType P2x2lt = new PieceType("2x2lt", 2, 2, false, new int[][] { { 0, 0 }, { 1, 0 }, { 0, 1 } }, null, new int[] { 2, 1, -1, 1, -1 });
+  public final static PieceType P2x2lb = new PieceType("2x2lb", 2, 2, false, new int[][] { { 1, 0 }, { 0, 1 }, { 1, 1 } }, null, new int[] { 1, -1, 1, 2, -2 });
 
-  public final static PieceType P2x2lr = new PieceType(2, 2, false, new int[][] { { 0, 0 }, { 1, 0 }, { 1, 1 } }, null, new int[] { 2, 2, -1, -1, -1 });
-  public final static PieceType P2x2ll = new PieceType(2, 2, false, new int[][] { { 0, 0 }, { 0, 1 }, { 1, 1 } }, null, new int[] { 1, 1, 1, 1, -2 });
+  public final static PieceType P2x2lr = new PieceType("2x2lr", 2, 2, false, new int[][] { { 0, 0 }, { 1, 0 }, { 1, 1 } }, null, new int[] { 2, 2, -1, -1, -1 });
+  public final static PieceType P2x2ll = new PieceType("2x2ll", 2, 2, false, new int[][] { { 0, 0 }, { 0, 1 }, { 1, 1 } }, null, new int[] { 1, 1, 1, 1, -2 });
 
   
-  public final static PieceType P4x2   = new PieceType(4, 2, false);
-  public final static PieceType P2x4   = new PieceType(2, 4, false);
+  public final static PieceType P4x2   = new PieceType("4x2", 4, 2, false);
+  public final static PieceType P2x4   = new PieceType("2x4", 2, 4, false);
   
-  public final static PieceType P8x2   = new PieceType(8, 2, false);
-  public final static PieceType P2x8   = new PieceType(2, 8, false);
+  public final static PieceType P8x2   = new PieceType("8x2", 8, 2, false);
+  public final static PieceType P2x8   = new PieceType("2x8", 2, 8, false);
   
   private final static PieceType[] pieces = new PieceType[] {
-    P1x1, P1x1r, P2x1, P1x2, P2x1r, P1x2r, P2x1c, P1x2c, P2x2, P2x2c, P2x2lt, P2x2lb, P2x2ll, P2x2lr, P3x1, P1x3, P3x1r, P1x3r, P4x1, P1x4, P6x1, P1x6, P4x2, P2x4, P8x2, P2x8
+    P1x1, P1x1r, 
+    P2x1, P1x2, P2x1r, P1x2r, P2x1c, P1x2c, 
+    P2x2, P2x2c, P2x2lt, P2x2lb, P2x2ll, P2x2lr, 
+    P3x1, P1x3, P3x1r, P1x3r, 
+    P4x1, P1x4, 
+    P6x1, P1x6, 
+    P4x2, P2x4, 
+    P8x2, P2x8
   };
   
   public final static PieceType[] spieces = new PieceType[] {
@@ -221,71 +230,40 @@ public class PieceType
     PieceType pt = rotations.get(piece);
     return pt != null ? pt : piece;
   }
+  
   private static void addRotation(PieceType p1, PieceType p2)
   {
     rotations.put(p1, p2);
     rotations.put(p2, p1);
   }
   
+  
   public static void initMapping()
   {
-    mapping.put("cap", CAP);
-
-    mapping.put("1x1", P1x1);
-    mapping.put("1x1r", P1x1r);
+    for (PieceType type : pieces)
+    {
+      mapping.put(type.ident, type);
+      mapping2.put(type, type.ident);
+    }
     
-    mapping.put("2x1", P2x1);
-    mapping.put("1x2", P1x2);
     addRotation(P2x1, P1x2);
-    
-    mapping.put("2x1c", P2x1c);
-    mapping.put("1x2c", P1x2c);
     addRotation(P2x1c, P1x2c);
-    
-    mapping.put("2x1r", P2x1r);
-    mapping.put("1x2r", P1x2r);
     addRotation(P2x1r, P1x2r);
     
-    mapping.put("3x1", P3x1);
-    mapping.put("1x3", P1x3);
     addRotation(P3x1, P1x3);
-    
-    mapping.put("3x1r", P3x1r);
-    mapping.put("1x3r", P1x3r);
     addRotation(P3x1r, P1x3r);
     
-    mapping.put("4x1", P4x1);
-    mapping.put("1x4", P1x4);
-    mapping.put("4x1r", P4x1r);
-    mapping.put("1x4r", P1x4r);
     addRotation(P4x1r, P1x4r);
     addRotation(P4x1, P1x4);
     
-    mapping.put("6x1", P6x1);
-    mapping.put("1x6", P1x6);
     addRotation(P6x1, P1x6);
     
-    mapping.put("2x2", P2x2);
-    mapping.put("2x2c", P2x2c);
-    
-    mapping.put("2x2lt", P2x2lt);
-    mapping.put("2x2lb", P2x2lb);
     addRotation(P2x2lt, P2x2lb);
-
-    mapping.put("2x2ll", P2x2ll);
-    mapping.put("2x2lr", P2x2lr);
     addRotation(P2x2ll, P2x2lr);
 
-    mapping.put("4x2", P4x2);
-    mapping.put("2x4", P2x4);
     addRotation(P4x2, P2x4);
-    
-    mapping.put("8x2", P8x2);
-    mapping.put("2x8", P2x8);
+
     addRotation(P8x2, P2x8);
-    
-    for (String s : mapping.keySet())
-      mapping2.put(mapping.get(s), s);
   }
   
   public static PieceType forName(String name) { return mapping.get(name); }
